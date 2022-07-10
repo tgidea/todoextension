a83b7aee79ab82dd9ba68a34e770919f909353af
Tushar <tushargoyal85709@gmail.com>
2022-07-10 09:21:43 +0530

    some ui improvement

--- popout.js
+++ popout.js
@@ -39,18 +39,23 @@ function fetchItems() {
             itemsArr = JSON.parse(itemsStorage);
         }
         var color1 = ['rgb(91, 121, 230)',' rgb(230, 91, 207)',' rgb(203, 186, 40)', 'rgb(213, 140, 58)'];
+        var color2 = 'rgb(206 202 202)';
         let x = Math.floor(Math.random()*10);
         // let idx = x%4;
         let idx = 0;
         for (var i = 0; i < itemsArr.length; i++) {
             console.log(idx);
-            var status = '';
+            var status = '' , color = `${color1[idx]};` , text = 'Done' , clor = 'white' , opq = 1;
             if (itemsArr[i].status == 1) {
                 status = 'class="done"';
+                color = `${color2}`;
+                text = 'Undo';
+                clor = '#958e8e;';
+                opq = 0.5;
             }
-            newItemHTML += `<li style="background-color : ${color1[idx]};" data-itemindex="${i}" ${status}>
-        <span class="item">${itemsArr[i].item}</span>
-        <div class="option"><button id="complete${i}" class="itemComplete">Done</button><button class="itemDelete">Delete</button></div>
+            newItemHTML += `<li style="background-color : ${color}; opacity:${opq}" data-itemindex="${i}" ${status}>
+        <span style="color:${clor}" class="item">${itemsArr[i].item}</span>
+        <div class="option"><button id="complete${i}" class="itemComplete">${text}</button><button class="itemDelete">Delete</button></div>
         </li>`;
         idx = idx +1;
         idx = idx%4;
@@ -75,7 +80,6 @@ function fetchItems() {
 }
 
 function itemComplete(index) {
-
     var itemsStorage = localStorage.getItem('todo-items');
     var itemsArr = JSON.parse(itemsStorage);
     console.log(itemsArr[index]);
@@ -90,7 +94,7 @@ function itemComplete(index) {
         document.getElementById(`complete${index}`).innerText = "Undo";
     }
     saveItems(itemsArr);
-
+    fetchItems();
 }
 function itemDelete(index) {
 
